ca2b4a8f7607f0a8735616f85118cafb2f4c7701
Prokopenko Viktor <pro100.viktor.w@gmail.com>
2025-05-30 16:50:57 +0300

    fix logout route

--- app/api/user/logout/route.ts
+++ app/api/user/logout/route.ts
@@ -1,3 +1,4 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 import { connectMongoDB } from '@/lib/mongodb';
@@ -5,22 +6,26 @@ import { connectMongoDB } from '@/lib/mongodb';
 export async function GET(request: Request, response: Response) {
   try {
     await connectMongoDB();
-    const newHeaders = new Headers(response.headers);
 
-    newHeaders.set(
-      'set-cookie',
-      `accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=Strict`,
-    );
-    newHeaders.append(
-      'set-cookie',
-      `refreshToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=Strict`,
-    );
-    return NextResponse.json(
-      { data: null, success: true, message: 'Вы вышли из аккаунта' },
-      {
-        headers: newHeaders,
-      },
-    );
+    const cookieStore = cookies();
+    cookieStore.set('accessToken', '', {
+      path: '/',
+      expires: new Date(0), // Удалить
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+    cookieStore.set('refreshToken', '', {
+      path: '/',
+      expires: new Date(0),
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+
+    return NextResponse.json({
+      data: null,
+      success: true,
+      message: 'Вы вышли из аккаунта',
+    });
   } catch (error: any) {
     return NextResponse.json(
       { success: false, message: 'Что-то пошло не так...', error: error.message },
