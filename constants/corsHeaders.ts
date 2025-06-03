feb425c6cd6cb379af6132c377857ff59ce49350
Prokopenko Viktor <pro100.viktor.w@gmail.com>
2025-06-03 15:56:44 +0300

    test

--- constants/corsHeaders.ts
+++ constants/corsHeaders.ts
@@ -6,7 +6,11 @@ export const getCorsHeaders = (origin: string | null) => {
   const isAllowed = origin && allowedOrigins.includes(origin);
 
   return {
-    'Access-Control-Allow-Origin': isDev ? process.env.DEV_ORIGIN || '*' : isAllowed ? origin : '',
+    'Access-Control-Allow-Origin': isDev
+      ? process.env.DEV_ORIGIN || '*'
+      : isAllowed
+        ? origin
+        : allowedOrigins[0]!,
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     'Access-Control-Allow-Credentials': 'true',
