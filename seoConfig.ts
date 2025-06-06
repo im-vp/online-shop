2a8600349ed208b5da6ef1ac8a87dac15596e331
Prokopenko Viktor <pro100.viktor.w@gmail.com>
2025-06-06 15:17:19 +0300

    added seo-config with metadata

---
+++ seoConfig.ts
@@ -0,0 +1,8 @@
+export const siteMetadata = {
+  main: {
+    title: process.env.NEXT_PUBLIC_PROJECT_NAME || 'Online Shop',
+    description:
+      'Онлайн-магазин с широким ассортиментом товаров, удобной навигацией и безопасной оплатой.',
+    keywords: 'онлайн магазин, купить товары, доставка, скидки',
+  },
+};
